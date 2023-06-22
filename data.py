d53ce6c0fae62c49d67332df04ecd8bf79eca0a1
kurtis <kurtisangell@gmail.com>
2023-06-22 20:22:48 +0100

    get athelte hrefs

--- data.py
+++ data.py
@@ -2,13 +2,17 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_athletes():
+def get_athletes_href_links():
     response = requests.get("https://www.flograppling.com/people")
     soup = BeautifulSoup(response.text, "html.parser")
-    elements = soup.find_all("h2")
-    for element in elements:
-        data = element.text
-        print(data)
+    all_a_tags = soup.find_all('a')
+
+    athlete_href_links = []
+
+    for link in all_a_tags:
+        href = link.get('href')
+        if isinstance(href, str) and href.startswith('/people/'):
+            athlete_href_links.append(href)
 
 
-get_athletes()
+get_athletes_href_links()
