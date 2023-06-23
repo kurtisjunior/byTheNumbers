964d450cf4a3c78c6e74c65caa96f00fd388978c
kurtis <kurtisangell@gmail.com>
2023-06-23 15:22:04 +0100

    some date logic

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 
 def get_athletes_href_links():
@@ -14,5 +15,21 @@ def get_athletes_href_links():
         if isinstance(href, str) and href.startswith('/people/'):
             athlete_href_links.append(href)
 
+    return athlete_href_links
 
-get_athletes_href_links()
+
+def get_match_data():
+    response = requests.get("https://www.flograppling.com/people/5950131-adam-wardzinski")
+    soup = BeautifulSoup(response.text, "html.parser")
+    # all_a_tags = soup.find('flo-table')
+
+    dates = soup.find_all('h5')
+    dates_as_strings = [str(year.string) for year in dates]
+
+    pattern = r"\b\d{4}\b"
+    valid_years = [year for year in dates_as_strings if re.match(pattern, year)]
+
+    print(valid_years)
+
+
+get_match_data()
