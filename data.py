a8d033480099b14584b47c940405af3ba29a4b29
kurtis <kurtisangell@gmail.com>
2023-06-27 14:45:50 +0100

    remove /people/ from athlete links

--- data.py
+++ data.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import re
 
 
-def get_athletes():
+def get_athletes_links():
     response = requests.get("https://www.flograppling.com/people")
     soup = BeautifulSoup(response.text, "html.parser")
     all_a_tags = soup.find_all('a')
@@ -13,16 +13,20 @@ def get_athletes():
     for link in all_a_tags:
         href = link.get('href')
         if isinstance(href, str) and href.startswith('/people/'):
-            athlete_href_links.append(href)
+            athlete_link = href.replace('/people/', "")
+            athlete_href_links.append(athlete_link)
 
-    return athlete_href_links
+    print(athlete_href_links)
+
+
+# return athlete_href_links
 
 
 def get_fight_data(athlete_link):
     response = requests.get("https://www.flograppling.com/people/" + athlete_link)
     soup = BeautifulSoup(response.text, "html.parser")
 
-    dates = s¡oup.find_all('h5')
+    dates = soup.find_all('h5')
     dates_as_strings = [str(year.string) for year in dates]
     pattern = r"\b\d{4}\b"
     valid_years = [year for year in dates_as_strings if re.match(pattern, year)]
@@ -44,16 +48,25 @@ def get_fight_data(athlete_link):
     return sub_results
 
 
-def extract():
-    athletes = get_athletes()
-    for athlete in athletes:
-        athlete_name = get_name(athlete)
-
-
 def get_name(athlete_name):
     split_name = athlete_name.split('-')
     del split_name[0]
     return ' '.join(split_name)
 
 
-get_name()
+def extract():
+    # athletes_links = get_athletes_links()
+    athletes_links = ["/5950131-adam-wardzinski", "5951184-andre-galvao"]
+
+    athlete_data = {}
+
+    for athlete_link in athletes_links:
+        athlete_name = get_name(athlete_link)
+        athlete_fight_data = get_fight_data(athlete_link)
+        athlete_data[athlete_name] = athlete_fight_data
+
+    print(athlete_data)
+    # return athlete_data
+
+
+get_athletes_links()
